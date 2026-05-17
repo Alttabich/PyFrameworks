0ee5e4e4e46da2e71beabbc73ad4516dcc9906b9
Alttabich <dmitrayxd@gmail.com>
2026-05-17 05:14:41 +0300

    ___

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/PyFrameworks/',
+
   plugins: [
-    UnoCSS()
-  ]
+    UnoCSS(),
+  ],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        django: resolve(__dirname, 'django.html'),
+        fastapi: resolve(__dirname, 'fastapi.html'),
+        flask: resolve(__dirname, 'flask.html'),
+        litestar: resolve(__dirname, 'litestar.html'),
+        compare: resolve(__dirname, 'compare.html'),
+      },
+    },
+  },
 })
